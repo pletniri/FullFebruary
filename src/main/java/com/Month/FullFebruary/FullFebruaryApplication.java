48652c04905fc836c55b7209cc6ac2117e6ba947
pletniri <pletniri@gmail.com>
2023-02-17 09:46:29 -0500

    initial commit

---
+++ src/main/java/com/Month/FullFebruary/FullFebruaryApplication.java
@@ -0,0 +1,13 @@
+package com.Month.FullFebruary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullFebruaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullFebruaryApplication.class, args);
+	}
+
+}
