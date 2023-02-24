ffd72f453193eae7df0352fbbc01c52bb24d83d9
pletniri <pletniri@gmail.com>
2023-02-24 16:53:54 -0500

    js delete and update

--- src/frontend/build/js/employeeById.js
+++ src/frontend/build/js/employeeById.js
@@ -5,8 +5,12 @@ let form = document.getElementById("product-form");
 
 async function findByIdEmployee(event) {
   event.preventDefault();
+  //The idProduct instance of EmployeeController calls its findById method with the value of the first form input field (form[0].value) converted to a number using the Number() function. The result of the method call is assigned to a variable called returnedValue.
   const returnedValue = await idProduct.findById(Number(form[0].value));
+  //Number() is a built-in function in JavaScript that converts a value to a number. It can be used to convert a variety of types to a numeric value, including:
   console.log(returnedValue);
+
+  //The values of the form input fields are set to the properties of the returnedValue object.
   form[0].value = returnedValue.id;
   form[1].value = returnedValue.name;
   form[2].value = returnedValue.position;
